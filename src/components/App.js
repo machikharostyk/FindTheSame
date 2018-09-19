c1288b4d7db6ff3314ea868b6e626de12fa52b70
Rostyk Machikha <finessq@gmail.com>
2018-09-19 12:36:59 +0300

    modify through react

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,40 @@
+import React, {Component} from 'react';
+
+
+
+class App extends Component{
+    state = {
+        isOver : false,
+    }
+    render(){
+        return(
+            <button onClick = {createGame}>Start Game</button>
+        )
+    }
+}
 function createGame(){
 
     //              Mix color array
     let colorArr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8], 
-		j, 
-		x, 
-		y;
+        j, 
+        x, 
+        y;
     for(y = colorArr.length; y;){
-		j = Math.floor(Math.random() * y);
-		x = colorArr[--y];
-		colorArr[y] = colorArr[j];
-		colorArr[j] = x;
+        j = Math.floor(Math.random() * y);
+        x = colorArr[--y];
+        colorArr[y] = colorArr[j];
+        colorArr[j] = x;
     }
     // create board and tiles
     for(j = 0; j < 16; j++) {
-		document.getElementById('root').innerHTML += '<a color="' + colorArr[j] + '" class="color' + colorArr[j] + ' hidden" color=""> </a>';
-	}
+        document.getElementById('root').innerHTML += '<a color="' + colorArr[j] + '" class="color' + colorArr[j] + ' hidden" color=""> </a>';
+       
+    }
     let isSame = false, 
-		currentColor = 0, 
-		currentEl, 
-		isOpen = 0,
-		a = document.getElementsByTagName('a');
+        currentColor = 0, 
+        currentEl, 
+        isOpen = 0,
+        a = document.getElementsByTagName('a');
     for(let i = 0; i < a.length; i++){
         // add click function to tiles
         a[i].addEventListener('click', function(event){
@@ -30,32 +45,29 @@ function createGame(){
                 if(isSame){
                     isSame = false;
                     // if 2 color are the same
-                    if(currentTarget.getAttribute('color') == currentColor){
+                    if(currentTarget.getAttribute('color') === currentColor){
                         isOpen++;
                         // if number of matches are maximum
-                        if(isOpen == 8){
-                            alert('You win');
+                        if(isOpen === 8){
+                           alert('You win!');
                         }
                     }
                     else{
                         // if colors arent the same
                         currentEl.className += ' hidden'; 
-						currentTarget.className += ' hidden';
+                        currentTarget.className += ' hidden';
                     }
                 }
                 else{
                     // if clicked on first tile in a pair
                     currentColor = currentTarget.getAttribute('color'); 
-					currentEl = currentTarget; 
-					isSame = true;
+                    currentEl = currentTarget; 
+                    isSame = true;
                 }
                 // setTimeOut to see color of clicked tile
             }, 300);
         });
     }
 }
-createGame();
 
-
-
-
+export default App;
